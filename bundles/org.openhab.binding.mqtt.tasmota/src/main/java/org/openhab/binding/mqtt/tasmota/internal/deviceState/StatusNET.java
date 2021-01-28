8dd3f1aedb3de1e476c58108bb509380f2f8b1f5
Jörg Ostertag <Joerg.Ostertag@github.com>
2021-01-28 22:03:36 +0100

    Add copyright and author comments

--- bundles/org.openhab.binding.mqtt.tasmota/src/main/java/org/openhab/binding/mqtt/tasmota/internal/deviceState/StatusNET.java
+++ bundles/org.openhab.binding.mqtt.tasmota/src/main/java/org/openhab/binding/mqtt/tasmota/internal/deviceState/StatusNET.java
@@ -1,5 +1,24 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ *
+ */
 package org.openhab.binding.mqtt.tasmota.internal.deviceState;
 
+/**
+ * @author Daan Meijer - Initial contribution
+ * @author Jörg Ostertag - Modifications to compile with openhab-3.1.0
+ *
+ */
 public class StatusNET {
 
     public String Hostname; // ""ESP-Pow-1"
--- bundles/org.openhab.binding.mqtt.tasmota/src/main/java/org/openhab/binding/mqtt/tasmota/internal/deviceState/StatusPTH.java
+++ bundles/org.openhab.binding.mqtt.tasmota/src/main/java/org/openhab/binding/mqtt/tasmota/internal/deviceState/StatusPTH.java
@@ -1,7 +1,26 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ *
+ */
 package org.openhab.binding.mqtt.tasmota.internal.deviceState;
 
 import java.util.List;
 
+/**
+ * @author Daan Meijer - Initial contribution
+ * @author Jörg Ostertag - Modifications to compile with openhab-3.1.0
+ *
+ */
 public class StatusPTH {
 
     public List<Integer> PowerDelta; // ; // "[0,0,0]
