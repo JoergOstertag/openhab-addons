45aa77ce63454226e0a87bd94b98d458cb085802
Jörg Ostertag <Joerg@github.de>
2021-06-12 11:38:00 +0200

    Add Package Info

---
+++ bundles/org.openhab.binding.mqtt.tasmota/src/main/java/org/openhab/binding/mqtt/tasmota/discovery/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ * <p>
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ * <p>
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ * <p>
+ * SPDX-License-Identifier: EPL-2.0
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * NOTE: This package contains stuff exclusively used for discovery
+ *
+ * @author Jörg Ostertag - Initial contribution
+ */
+package org.openhab.binding.mqtt.tasmota.discovery;
